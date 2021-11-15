3f8d50be202ed1a6c8aea9c05990f0c875aea98d
Mihir Gadgil <16473290+scimas@users.noreply.github.com>
2021-11-15 13:40:55 -0700

    Add serialization

---
+++ src/druid_query/utils.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from .components.granularities import Simple
+from .components.intervals import Interval
+
+
+def camelCase(s: str):
+    parts = s.split('_')
+    return parts[0] + ''.join(word.capitalize() for word in parts[1:])
+
+
+def druid_serealize(obj) -> Union[dict, str]:
+    if isinstance(obj, Simple):
+        return obj.name
+    if isinstance(obj, Interval):
+        return obj.start + '/' + obj.end
+    out_dict = {}
+    for i in dir(obj):
+        if i.startswith('__') or callable(getattr(obj, i)):
+            continue
+        attr = getattr(obj, i)
+        if attr is not None:
+            key = camelCase(i)
+            out_dict[key] = attr
+    return out_dict
